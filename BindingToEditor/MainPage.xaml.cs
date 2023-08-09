7c559160bc47ef608b83efdaab298ae1289eab95
SamuelJames101 <SamuelJames101101@gmail.com>
2023-08-09 14:33:16 +0100

    inital commit

---
+++ BindingToEditor/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿namespace BindingToEditor;
+
+public partial class MainPage : ContentPage
+{
+	MainPageViewModel vm;
+
+    public MainPage()
+	{
+		InitializeComponent();
+
+		this.BindingContext = this.vm = new MainPageViewModel();
+
+		this.vm.ChangeFromCodeBehind += () =>
+		{
+            myEditor.Text = "new text";
+        };
+	}
+
+}
+
+
